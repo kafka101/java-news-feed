ecd3165a483e733691cd196fa3233e79b84cf8e9
Lars Pfannenschmidt <lars@d8a.wtf>
2016-04-10 15:04:15 +0200

    Minor update to example and API import

--- producer/src/main/java/io/github/kafka101/newsfeed/producer/NewsProducer.java
+++ producer/src/main/java/io/github/kafka101/newsfeed/producer/NewsProducer.java
@@ -1,33 +1,30 @@
 package io.github.kafka101.newsfeed.producer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.kafka101.newsfeed.domain.News;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
- *
+ * This class is a simple kafka producer using the 0.9.x API. It provides a synchronous and an asynchronous method to
+ * push messages to a kafka broker.
  */
 public class NewsProducer {
-    private static final Logger logger = LoggerFactory.getLogger(NewsProducer.class);
     private final String producerName;
     private final String topic;
     private final KafkaProducer<String, News> producer;
-    private final ObjectMapper mapper = new ObjectMapper();
 
     /**
      * @param producerName
      * @param topic
-     * @param broker       {@value ProducerConfig#BOOSTRAP_SERVERS_DOC}
+     * @param broker    {@value CommonClientConfigs#BOOSTRAP_SERVERS_DOC}
      */
     public NewsProducer(String producerName, String topic, String broker) {
         this.producerName = producerName;
@@ -37,6 +34,7 @@ public class NewsProducer {
 
     private KafkaProducer<String, News> createProducer(String broker) {
         Properties config = new Properties();
+        config.put(ProducerConfig.CLIENT_ID_CONFIG, this.producerName);
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, NewsSerializer.class.getName());
